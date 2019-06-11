99f3b75f51292c9c8faf905d8998d2aad48fc939
Jonathan Saylor <jsaylor525@gmail.com>
2019-06-11 10:59:29 -0700

    Copied cost function from original

--- week_2/machine-learning-ex1/ex1/computeCostMulti.m
+++ week_2/machine-learning-ex1/ex1/computeCostMulti.m
@@ -12,8 +12,8 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
+hypothesis = theta' * X';
+J = (1/(2*m)) * sum((hypothesis' - y).^2);
 
 
 

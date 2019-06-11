1d54dca41de77205debf8d53320bb17ec3e4f985
Jonathan Saylor <jsaylor525@gmail.com>
2019-06-11 10:05:30 -0700

    Implemented cost function

--- week_2/machine-learning-ex1/ex1/computeCost.m
+++ week_2/machine-learning-ex1/ex1/computeCost.m
@@ -12,11 +12,24 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+if 1,
+    % Vectorized hypothesis computation, h = theta^T * x (X needed to be transposed because of input dimensions) 
+    hypothesis = theta' * X';
+    J = (1/(2*m)) * sum((hypothesis' - y).^2);
+else,
+    % Manual
+    cost_sum = 0;
+    hypothesis = 0;
+    for i = 1:m
+        hypothesis = theta' * X(1,:)';
+        %  hypothesis = theta(1) + theta(2)* X(1,:);
+        %  fprintf('Hypothesis at iteration %i = %f\n', i, hypothesis(i));
+        cost_sum = cost_sum + (hypothesis(i) - y(i))^2;
+    end
 
-
-
-
-
+    J = (1/(2*m)) * cost_sum
+    %fprintf('Cost computed = %f\n', J);
+end
 % =========================================================================
 
 end
